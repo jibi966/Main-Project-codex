b10bea7dba25b8f4df5dd9468e9a7dd9bbd14529
Jibin <jibin@empplan.com>
2026-02-09 19:33:56 +0400

    updated frontend url

--- backend/server.js
+++ backend/server.js
@@ -5,9 +5,14 @@ require("dotenv").config();
 
 const server = http.createServer(app);
 
+const allowedOrigins = [
+  "http://localhost:3000",
+  "https://main-project-codex-ov1i.vercel.app",
+];
+
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:3000",
+    origin: allowedOrigins,
     methods: ["GET", "POST"],
   },
 });
